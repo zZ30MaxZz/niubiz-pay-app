a87f3dace7993a2c99e69ca89aebd30fdb4a23cb
Max Aguilar <zz30.maxzz@gmail.com>
2025-03-12 11:49:26 -0500

    feat(app-form) adding forms to pay and tokenize cards to apps

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/page';
 import PayPage from 'pages/pay/page';
+import PayAppPage from 'pages/pay-app/page';
+import TokenAppPage from 'pages/token-app/page';
 
 const App: React.FC = () => {
     return (
@@ -9,6 +11,8 @@ const App: React.FC = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/pay" element={<PayPage />} />
+                <Route path="/pay-app" element={<PayAppPage />} />
+                <Route path="/token-app" element={<TokenAppPage />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/pay-app/page.tsx
@@ -0,0 +1,111 @@
+import Loader from 'components/Loader/Loader';
+import { MerchantDefineData } from 'components/types';
+import useNiubizPayApp from 'hooks/useNiubizPayApp';
+import React, { useEffect, useMemo } from 'react'
+import { MoonLoader } from 'react-spinners';
+
+const PayAppPage = () => {
+    const [sessionKey, setsessionKey] = React.useState<string | null>(null);
+    const [token, setToken] = React.useState<string | null>(null);
+    const [merchantId, setMerchantId] = React.useState<string | null>(null);
+
+    const amount = "199.9";
+
+    const MDD: MerchantDefineData = useMemo(() => ({
+        MDD4: 'mail@mail.com',
+        MDD32: '12345789',
+        MDD75: 'Registrado',
+        MDD77: '0',
+    }), []);
+
+    const generateToken = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const bodyData = {
+            Channel: "web",
+            Amount: amount,
+            Currency: "PEN",
+        };
+
+        try {
+            const response = await fetch("https://6s5x6zwwg3.execute-api.us-east-1.amazonaws.com/dev/api.payments/v1/payment/tokenize", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(bodyData),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
+
+            const data: {
+                data: {
+                    sessionKey: string;
+                    expirationTime: number;
+                    authorization: string;
+                    merchantId: string;
+                };
+                isSuccess: boolean;
+                message: string;
+            } = await response.json();
+
+            if (data.isSuccess) {
+                setToken(data.data.authorization);
+                setsessionKey(data.data.sessionKey);
+                setMerchantId(data.data.merchantId);
+            } else {
+                console.error("Request failed with message:", data.message);
+            }
+        } catch (error) {
+            console.error("Request failed:", (error as Error).message);
+        }
+    };
+
+    const { FormComponent, triggerOpenForm, triggerSendForm, formResponse, triggerResetForm } = useNiubizPayApp(
+        "userniubiz@mail.com",
+        "0",
+        "44554444",
+        Math.floor(Math.random() * 120000) + 1,
+        "https://apisandbox.vnforappstest.com",
+        "/api.security/v1/security",
+        "/api.ecommerce/v2/ecommerce/token/session",
+        "/api.authorization/v3/authorization/ecommerce",
+        "https://pocpaymentserve.s3.amazonaws.com/payform.min.js",
+        "https://pocpaymentserve.s3.amazonaws.com/payform.min.css",
+        MDD,
+        "web",
+        "web",
+        amount,
+        "Z2lhbmNhZ2FsbGFyZG9AZ21haWwuY29tOkF2MyR0cnV6",
+        merchantId ?? "110777209",
+        token,
+        sessionKey,
+        <Loader loader={MoonLoader} color='white' />
+    );
+
+    useEffect(() => {
+        console.log('Respuesta del formulario PAY APP 😁', formResponse);
+
+    }, [formResponse])
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <strong>Pay page</strong>
+                </div>
+                <button onClick={generateToken}>Generar Token y Session</button>
+                <div>TOKEN: {token}</div>
+                <div>SESSION: {sessionKey}</div>
+                <div>Form</div>
+                {FormComponent}
+            </div>
+            <button onClick={triggerOpenForm}>Open Form</button>
+            <button onClick={triggerResetForm}>Reset Form</button>
+            <button onClick={triggerSendForm}>Send Form</button>
+        </div >
+    )
+}
+export default PayAppPage
